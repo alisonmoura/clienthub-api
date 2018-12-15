1742af708522b133f7359add19549e7f60d6efdd
Alison Moura <alison.oghino@gmail.com>
2018-12-15 15:35:42 -0300

    Atualizando DataBaseConnection para Heroku

--- connection/DataBaseConnection.js
+++ connection/DataBaseConnection.js
@@ -5,24 +5,29 @@ const config = require('./../config')
 
 module.exports = class DataBaseConnection {
     static async connect() {
-        if (!config.db) throw {
-            message: "DataBaseConnection: o arquivo de configuração deve ter as configurações do banco de dados"
-        }
-        if (!config.db.host) throw {
+        if (!config.db.host && !process.env.DB_HOST) throw {
             message: "DataBaseConnection: o arquivo de configuração deve ter informar o host de conexção com o banco de dados em db.host"
         }
-        if (!config.db.port) throw {
+        if (!config.db.port && !process.env.DB_PORT) throw {
             message: "DataBaseConnection: o arquivo de configuração deve ter informar a porta de conexção com o banco de dados em db.port"
         }
-        if (!config.db.name) throw {
+        if (!config.db.name && !process.env.DB_NAME) throw {
             message: "DataBaseConnection: o arquivo de configuração deve ter informar o nome do banco de dados em db.name"
         }
 
+        let db = {
+            host: config.db.host || process.env.DB_HOST,
+            port: config.db.port || process.env.DB_PORT,
+            name: config.db.name || process.env.DB_NAME,
+            user: config.db.user || process.env.DB_USER,
+            password: config.db.password || process.env.DB_PASS
+        }
+
         try {
-            if (config.db.user && config.db.password) return await Mongoose.connect(`mongodb://${config.db.user}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.name}`, {
+            if (db.user && db.password) return await Mongoose.connect(`mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}`, {
                 useNewUrlParser: true
             })
-            else return await Mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, {
+            else return await Mongoose.connect(`mongodb://${db.host}:${db.port}/${db.name}`, {
                 useNewUrlParser: true
             });
         } catch (error) {
